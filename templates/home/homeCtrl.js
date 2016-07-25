a222d97594126ebf408913e9790897b3e6292b46
mckmillions <zachmckimmins@gmail.com>
2016-07-25 09:47:54 -0500

    home view is mainly styled and semi functioning

--- templates/home/homeCtrl.js
+++ templates/home/homeCtrl.js
@@ -1,42 +1,78 @@
 angular.module( "expApp" ).controller( "homeCtrl", function( $scope, $state, $cookies ) {
 
 	$scope.user = $cookies.getObject( "user" );
+	$scope.pending = [];
+	$scope.reimbursed = [];
+	$scope.edit = true;
 
 	if ( !$scope.user ) {
 		$state.go( "login" );
 		return "Unauthorized", "Please log in", "error";
 	}
 
-	console.log( "user(homeCtrl):", $scope.user );
+	// console.log( "user(homeCtrl):", $scope.user );
 
-	console.log( $scope.user[ "first" ] );
+	// console.log( $scope.user[ "first" ] );
 
 	$scope.logout = function() {
 		$scope.user = {};
 		$cookies.remove( "user" );
-		if ( $scope.user === {} ) {
+		if ( !$scope.user.length ) {
 			$state.go( "login" );
-		} console.log( "logout", $scope.user );
+		}
 	};
+
+	$scope.getExpenses = function() {
+		// $scope.expenses = $cookies.getObject( "newExpense" );
+		console.log( "getExpenses", $scope.expenses );
+		$scope.pending = [];
+		$scope.reimbursed = [];
+
+		// $scope.expenses.forEach( function( element ) {
+		// 	if ( element.dateReimbursed === null ) {
+		// 		$scope.pending.push( element );
+		// 	} else {
+		// 		$scope.reimbursed.push( element );
+		// 	}
+		// } );
+	};
+	// $scope.getExpenses();
+
+	$scope.expenses = [];
 
 	$scope.addUserExpense = function() {
-
-		$scope.newExpense;
-		expenseService.addUserExpense( $scope.newExpense, $scope.user._id )
-			.then( function( res ) {
-				$scope.getUserExpenses();
-				$scope.expense.$setUntouched();
-				$scope.newExpense = {};
-			} );
+		// $cookies.putObject( "newExpense", $scope.newExpense );
+		$scope.expenses.push( $scope.newExpense );
+		// console.log( "newExpense: ", $scope.newExpense );
+		$scope.getExpenses();
+		$scope.newExpense = {};
+		$scope.expense.$setUntouched();
 	};
 
-	// $scope.createUser = function() {
-	//   if ($scope.userInfo.password === $scope.userInfo.passwordTwo) {
-	//     console.log("userInfo", $scope.userInfo);
-	//     $cookies.put('user', $scope.userInfo);
-	//         $state.go('home');
-	//   } else {
-	//     console.log("Passwords Do Not Match", "Please check to be sure your passwords match.");
-	//   }
-	// };
+	$scope.rmbAlert = function( expense ) {
+
+    																				console.log( "expense(rmbAlert)", expense );
+
+		$scope.expId = expenseId;
+
+// 		function( inputValue ) {
+// 		if ( !inputValue ) {
+// 			console.log( "You need to enter the date of reimbursement!" );
+// 			return false;
+// 		}
+// 		$scope.rmbDate = new Date( inputValue );
+// 		if ( !$scope.rmbDate.valueOf() ) {
+// 			console.log( "You need to enter a valid date!" );
+// 			return false;
+// 		}
+// 		if ( $scope.rmbDate.valueOf() ) {
+// 			console.log( "Reimbursed!", "Reimbursement Date: " + inputValue, "success" );
+// 			expenseService.reimburse( $scope.rmbDate, $scope.expId )
+// 				.then( function( ) {
+// 					$scope.getUserExpenses();
+// 				} );
+// 		}
+// 	};
+	};
+
 } );
--- templates/login/loginCtrl.js
+++ templates/login/loginCtrl.js
@@ -1,32 +1,32 @@
 angular.module( "expApp" ).controller( "loginCtrl", function( $scope, $state, $cookies ) {
+
 	$scope.IsVisible = false;
 	$scope.ShowHide = function() {
 		//If DIV is visible it will be hidden and vice versa.
 		$scope.IsVisible = $scope.IsVisible ? false : true;
-	}
+	};
 
-  // $scope.createUser = function() {
-  //   if ($scope.userInfo.password === $scope.userInfo.passwordTwo) {
-  //     console.log("userInfo", $scope.userInfo);
-  //     loginSrvc.createUser($scope.userInfo)
-  //       .then(function(res) {
-  //         console.log("res", res);
-  //         swal("Created New User", "You are now logged in.", "success")
-  //         $state.go('main');
-  //       });
-  //   } else {
-  //     swal("Passwords Do Not Match", "Please check to be sure your passwords match.", "error")
-  //   }
-  // };
+	// $scope.createUser = function() {
+	//   if ($scope.userInfo.password === $scope.userInfo.passwordTwo) {
+	//     console.log("userInfo", $scope.userInfo);
+	//     loginSrvc.createUser($scope.userInfo)
+	//       .then(function(res) {
+	//         console.log("res", res);
+	//         swal("Created New User", "You are now logged in.", "success")
+	//         $state.go('main');
+	//       });
+	//   } else {
+	//     swal("Passwords Do Not Match", "Please check to be sure your passwords match.", "error")
+	//   }
+	// };
 
-  $scope.createUser = function() {
-    if ($scope.userInfo.password === $scope.userInfo.passwordTwo) {
-      console.log("userInfo", $scope.userInfo);
-      $cookies.putObject('user', $scope.userInfo);
-          $state.go('home');
-    } else {
-      console.log("Passwords Do Not Match", "Please check to be sure your passwords match.");
-    }
-  };
-
+	$scope.createUser = function() {
+		if ( $scope.userInfo.password === $scope.userInfo.passwordTwo ) {
+			// console.log( "userInfo", $scope.userInfo );
+			$cookies.putObject( "user", $scope.userInfo );
+			$state.go( "home" );
+		} else {
+			return "Passwords Do Not Match, Please check to be sure your passwords match.";
+		}
+	};
 } );
